1220f90b771503e81c13b161f1371e1a12fc6d19
Antonio Ribeiro <alvesjunior.antonio@gmail.com>
2011-06-01 18:58:05 -0300

    First commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0.1'
+
+setup(name='bibishare',
+      version=version,
+      description="",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Antonio Ribeiro',
+      author_email='alvesjunior.antonio@gmail.com',
+      url='',
+      license='GNU',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
