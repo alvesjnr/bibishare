3b61e57d3052cc05025ac47e4c0347a509956f64
Antonio Ribeiro Alves Júnior <alvesjunior.antonio@gmail.com>
2011-06-14 21:52:08 -0300

    adding login view

--- bibishare/models/users.py
+++ bibishare/models/users.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.orm import scoped_session, sessionmaker
 from datetime import datetime
+from Crypto.Hash import SHA256
 
 from bibishare.models import Base
 
@@ -17,10 +18,12 @@ class User(Base):
     fullname = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     email = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     password = sqlalchemy.Column(sqlalchemy.String, )
+    password_encryption = sqlalchemy.Column(sqlalchemy.String, )
     
     def __init__(self, username, password, fullname, email, group=None):
         self.username = username
-        self.password = password
+        self.password = SHA256.new(password).hexdigest()
+        self.password_encryption = 'SHA256'
 
         self.fullname = fullname
         self.email = email
