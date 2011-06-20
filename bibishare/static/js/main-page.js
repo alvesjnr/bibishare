e7454fa37673ad11084a3bb23a5483570f95b58e
Antonio Ribeiro <alvesjunior.antonio@gmail.com>
2011-06-20 11:31:52 -0300

    improving list sorting using jquery

--- bibishare/static/js/main-page.js
+++ bibishare/static/js/main-page.js
@@ -1,5 +1,3 @@
-$(document).ready(function() 
-    { 
-        $("#myTable").tablesorter( {sortList: [[0,0], [1,0]]} ); 
-    } 
-); 
+$(document).ready(function() { 
+    $("#articles-table").tablesorter();
+}); 
